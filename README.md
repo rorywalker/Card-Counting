148006e2b38afabdaddc1caf4b73014462d453b9
rorywalker <r1982walker@hotmail.com>
2017-07-11 05:27:18 -0600

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+This is to help a person while playing Blackjack. If the count is greater than or equal to four, the person should increase the bet amount because there are significantly more higher value cards remaining within the shoe. Likewise, if the count falls below four, the player should reduce the bet amount to a standard amount. 
+
---
+++ app.js
@@ -0,0 +1,121 @@
+
+
+
+(function () {
+
+    let deck = [
+		'2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+		'2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
+		'2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+		'2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD'
+	];
+
+    let inventory = [];
+
+    var btnPlay = document.getElementById('btnPlay');
+        btnPlay.addEventListener('click', play);
+
+    var btnNextCard = document.getElementById('btnNextCard');
+        btnNextCard.addEventListener('click', nextCard);
+
+    var btnShowCount = document.getElementById('btnShowCount');
+        btnShowCount.addEventListener('click', showCount)
+
+    var cardDisplay = document.getElementById('cardDisplay');
+    var card1 = document.getElementById('card1');
+
+    var countDisplay = document.getElementById('countDisplay');   
+
+    function play () {
+        shuffle (deck);
+
+        btnPlay.classList.add('hidden');
+        btnNextCard.classList.remove('hidden');
+        btnShowCount.classList.remove('hidden');
+        cardDisplay.classList.remove('hidden');
+
+        inventory.push(deck.shift());
+        card1.src = 'img/' + inventory[0] + '.png';
+    }
+
+    var count = 1;
+    
+    function nextCard () {
+        btnShowCount.removeAttribute("disabled")
+            inventory.push(deck.shift());
+                    card1.src = 'img/' + inventory[count] + '.png';
+                        //console.log(inventory);
+                        count++;
+                           if (count == 291) {
+                                btnNextCard.classList.add('hidden');
+                                btnShowCount.classList.add('hidden');
+                                cardDisplay.classList.add('hidden');
+                            }
+    }
+
+    function showCount () {
+
+        var currentCount = 0;
+
+        btnShowCount.setAttribute("disabled", "nothing");
+
+        for (var i = 0; i < inventory.length; i++) {
+            tempCard0 = inventory[i].replace(/[CDHS]/gi, '');
+                if (('AKQJ').includes(tempCard0)) {
+                    currentCount -= 1;
+                } else {
+                    tempCard1 = parseInt(tempCard0);
+                        if (tempCard1 == 10) {
+                            currentCount -= 1;
+                        } else if (tempCard1 == 7 || tempCard1 == 8 || tempCard1 == 9) {
+                            currentCount += 0;
+                        } else {
+                            currentCount += 1;
+                        }
+                }
+            }
+        countDisplay.innerHTML = '<strong>' + currentCount + '</strong>';        
+    }                                                           
+
+    function shuffle(array) {
+		var m = array.length, t, i;
+    		while (m) {
+    			i = Math.floor(Math.random() * m--);
+        			t = array[m];
+		        	array[m] = array[i];
+			        array[i] = t;
+		    }
+		return array;
+	}
+
+})();
+
+
